e9f9d403b0228905324aadfa3a5f2073523af8ce
Bianca A <frank@Bianca-Avila-4.local>
2018-10-30 12:03:32 +0900

    file

---
+++ init.rb
@@ -0,0 +1,13 @@
+#set the file path 
+
+# this one get the relative path
+APP_ROOT = File.dirname(__FILE__)
+
+#this is where the app will have the logic
+# require "{APP_ROOT/lib/guide}"
+
+# other practice . this one joins and looks for the directory lib 
+$:.unshift(File.join(APP_ROOT, 'lib'))
+
+# then we require the file 
+require 'guide'
---
+++ lib/guide.rb
@@ -0,0 +1,19 @@
+# guide class 
+class Guide 
+# this one has flexibility
+	def initialize(path=nil)
+		#locate the restaurant text file 
+		#o create the file 
+		#exit if create fails
+	end 
+
+	def launch!
+		#intro 
+		#action loop
+		#user action ( list, find , add , quit )
+		#do action 
+
+		#repeat until user quits 
+		#conclusion
+	end 
+end 
