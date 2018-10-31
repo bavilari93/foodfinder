fdd58a98c239f6905780d1476a3180bbaf0d75b6
Bianca A <frank@Bianca-Avila-4.local>
2018-10-31 18:28:48 +0900

    actions

--- lib/guide.rb
+++ lib/guide.rb
@@ -3,6 +3,12 @@ require 'restaurant'
 
 # guide class 
 class Guide 
+
+	class Config 
+		@@actions = ['list', 'find', 'add', 'quit']
+		# this one is to be able to access it from outside as a reader method
+		def self.actions; @@actions; end
+	end 
 # this one has flexibility
 	def initialize(path=nil)
 		#locate the restaurant text file 
@@ -24,15 +30,27 @@ class Guide
 		introduction
 		#action loop
 		#user action ( list, find , add , quit )
-		loop do
-			print">"
-			user_response = gets.chomp
-			result = do_action(user_response)
-			break if result == :quit
+		result = nil
+		until result == :quit
+			action = get_action
+			result = do_action(action)
 		end
 		#do action 
 		#repeat until user quits 
 		conclusion
+	end 
+
+	def get_action
+		action = nil
+		# keep asking for user input until we get a valid action
+		until Guide::Config.actions.include?(action)
+		# this one display the list of actions that the user can do
+			puts "actions :" + Guide::Config.actions.join(", ")if action
+			print">"
+			user_response = gets.chomp
+			action = user_response.downcase.strip
+		end
+			return action 
 	end 
 
 	def do_action(action)
@@ -46,7 +64,7 @@ class Guide
 		when "quit"
 			return :quit
 		else
-			puts "\n i don;t undertand that command"
+			puts "\n i don't undertand that command"
 		end
 	end
 
